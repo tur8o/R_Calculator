1821258dea1fa499c0508c513f327889d740c1c6
tur8o <robert.farrell@europcar.ie>
2017-05-12 20:42:17 +0100

    V1
    Testing and playing around with variables

---
+++ RFARRELL_10340158_CALC.R
@@ -0,0 +1,25 @@
+#Building a simple calculator in R Script
+
+addition <- function(x, y) {
+  return(x + y)
+}
+
+addition(1,2)
+
+subtraction <- function(x, y) {
+  return(x - y)
+}
+
+subtraction(1,2)
+
+division <- function(x, y) {
+  return(x / y)
+}
+
+division(1,2)
+
+multiplication <- function(x, y) {
+  return(x * y)
+}
+
+multiplication(1,2)
